804d5c47ea607b91d52c115f0fdf6f1de647044c
yhh52367y <yhh52367y@gmail.com>
2015-02-09 16:31:54 +0800

    first build

---
+++ ksana.js
@@ -0,0 +1,11 @@
+jsonp_handler({
+"version": "1",
+"build": 1,
+"title":"myclock",
+"minruntime": 1 ,
+"baseurl":"http://rawgit.com/yhh52367y/myclock/master/",
+"description":"",
+"files":
+  ["index.html","index.css","bundle.js","jquery.js","react-with-addons.js","ksana.js",
+  "bootstrap.min.js","bootstrap.min.css","sample.kdb"]
+})
